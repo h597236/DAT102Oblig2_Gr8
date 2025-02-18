765414103c7088c81b7a00152f2f124142d63312
Abdul Al kadri <187457@stud.hvl.no>
2025-02-18 14:06:35 +0100

    Oppgave 2 - comments deleted

--- src/no/dat/hvl102/veke7/oppg1/InsettingsSorteringA.java
+++ src/no/dat/hvl102/veke7/oppg1/InsettingsSorteringA.java
@@ -2,18 +2,15 @@ package no.dat.hvl102.veke7.oppg1;
 
 public class InsettingsSorteringA {
 
-    // Hjelpemetode for å bytte to elementer i en tabell
     private static <T> void swap(T[] a, int i, int j) {
         T temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
 
-    // Modifisert insertion sort: Flytt minste element først
     public static <T extends Comparable<T>> void insertionSort(T[] a) {
         int n = a.length;
 
-        // Finn og flytt det minste elementet til starten
         int minIndex = 0;
         for (int i = 1; i < n; i++) {
             if (a[i].compareTo(a[minIndex]) < 0) {
@@ -22,12 +19,10 @@ public class InsettingsSorteringA {
         }
         swap(a, 0, minIndex);
 
-        // Sorter resten av tabellen
         for (int i = 1; i < n; i++) {
             T temp = a[i];
             int j = i - 1;
 
-            // Fjernet sjekken j >= 0 siden det minste elementet allerede er på plass
             while (a[j].compareTo(temp) > 0) {
                 a[j + 1] = a[j];
                 j--;
@@ -36,7 +31,6 @@ public class InsettingsSorteringA {
         }
     }
 
-    // Hjelpemetode for å generere tilfeldige data
     public static Integer[] generateRandomData(int n) {
         Integer[] data = new Integer[n];
         for (int i = 0; i < n; i++) {
@@ -45,9 +39,8 @@ public class InsettingsSorteringA {
         return data;
     }
 
-    // Hovedmetode for testing
     public static void main(String[] args) {
-        int n = 100000; // Antall elementer
+        int n = 100000;
         Integer[] data = generateRandomData(n);
 
         long startTime = System.currentTimeMillis();
--- src/no/dat/hvl102/veke7/oppg1/InsettingsSorteringB.java
+++ src/no/dat/hvl102/veke7/oppg1/InsettingsSorteringB.java
@@ -2,19 +2,17 @@ package no.dat.hvl102.veke7.oppg1;
 
 public class InsettingsSorteringB {
 
-    // Hjelpemetode for å bytte to elementer i en tabell
     private static <T> void swap(T[] a, int i, int j) {
         T temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
 
-    // Modifisert insertion sort: Sett inn to elementer om gangen
+
     public static <T extends Comparable<T>> void insertionSort(T[] a) {
         int n = a.length;
 
         for (int i = 0; i < n; i += 2) {
-            // Håndter tilfelle med odde antall elementer
             if (i + 1 >= n) {
                 T last = a[i];
                 int j = i - 1;
@@ -27,7 +25,6 @@ public class InsettingsSorteringB {
                 break;
             }
 
-            // Velg to elementer og sørg for at min <= max
             T min = a[i];
             T max = a[i + 1];
             if (min.compareTo(max) > 0) {
@@ -38,16 +35,13 @@ public class InsettingsSorteringB {
 
             int j = i - 1;
 
-            // Flytt elementer to plasser til høyre så lenge max er mindre enn elementet i sortert del
             while (j >= 0 && a[j].compareTo(max) > 0) {
                 a[j + 2] = a[j];
                 j--;
             }
 
-            // Sett inn max
             a[j + 2] = max;
 
-            // Sett inn min
             while (j >= 0 && a[j].compareTo(min) > 0) {
                 a[j + 1] = a[j];
                 j--;
@@ -56,7 +50,6 @@ public class InsettingsSorteringB {
         }
     }
 
-    // Hjelpemetode for å generere tilfeldige data
     public static Integer[] generateRandomData(int n) {
         Integer[] data = new Integer[n];
         for (int i = 0; i < n; i++) {
@@ -65,9 +58,8 @@ public class InsettingsSorteringB {
         return data;
     }
 
-    // Hovedmetode for testing
     public static void main(String[] args) {
-        int n = 100000; // Antall elementer
+        int n = 100000;
         Integer[] data = generateRandomData(n);
 
         long startTime = System.currentTimeMillis();
